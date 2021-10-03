7557150d3c6710de11f45f994c9bcae99cc83a8c
Pranta07 <palpranta@gmail.com>
2021-10-04 02:24:33 +0600

    initial commit

---
+++ src/componenets/SingleCourse/SingleCourse.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleCourse = () => {
+    return (
+        <div>
+            <h4>Course</h4>
+        </div>
+    );
+};
+
+export default SingleCourse;
---
+++ src/componenets/Tutorials/Tutorials.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tutorials = () => {
+    return (
+        <div>
+            <h2>All Tutorials!</h2>
+        </div>
+    );
+};
+
+export default Tutorials;
