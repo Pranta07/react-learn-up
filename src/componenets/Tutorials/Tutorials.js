bf385fdf762257f337651beabb4bd073c24ec140
Pranta07 <palpranta@gmail.com>
2021-10-04 15:10:43 +0600

    youtube-tutorials-page added

--- src/componenets/Tutorials/Tutorials.js
+++ src/componenets/Tutorials/Tutorials.js
@@ -9,18 +9,30 @@ const Tutorials = () => {
             <h2 className="py-3 fw-bold text-center">
                 Find All Free Tutorials Here!
             </h2>
-            <Row sm lg="2">
-                <Col lg="6">
-                    <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
+            <Row className="g-3 pb-5">
+                <Col sm={6}>
+                    <ReactPlayer
+                        url="https://www.youtube.com/watch?v=Ke90Tje7VS0&t=5665s"
+                        width="200"
+                    />
                 </Col>
-                <Col lg="6">
-                    <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
+                <Col sm={6}>
+                    <ReactPlayer
+                        url="https://www.youtube.com/watch?v=W6NZfCO5SIk"
+                        width="200"
+                    ></ReactPlayer>
                 </Col>
-                <Col>
-                    <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
+                <Col sm={6}>
+                    <ReactPlayer
+                        url="youtube.com/watch?v=qz0aGYrrlhU"
+                        width="200"
+                    />
                 </Col>
-                <Col>
-                    <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
+                <Col sm={6}>
+                    <ReactPlayer
+                        url="https://www.youtube.com/watch?v=-_X6PhkjpzU"
+                        width="200"
+                    />
                 </Col>
             </Row>
         </Container>
