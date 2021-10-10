814e61d481f157ca5478ec73cfdc76b1912aded6
Pranta07 <palpranta@gmail.com>
2021-10-11 01:49:27 +0600

    notfound page bug fixed

--- src/componenets/NotFound/NotFound.js
+++ src/componenets/NotFound/NotFound.js
@@ -3,7 +3,11 @@ import React from "react";
 const NotFound = () => {
     return (
         <div>
-            <img src="https://i.stack.imgur.com/6M513.png" alt="" />
+            <img
+                src="https://i.stack.imgur.com/6M513.png"
+                alt=""
+                className="w-100"
+            />
         </div>
     );
 };
